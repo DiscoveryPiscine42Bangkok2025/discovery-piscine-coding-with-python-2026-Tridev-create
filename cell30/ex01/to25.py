1bb5ac4bb20fc51e3d01ae1a4425314e746e77d5
trikhun <trikhun@MSI.localdomain>
2026-02-12 01:16:33 +0700

    r

--- cell30/ex01/to25.py
+++ cell30/ex01/to25.py
@@ -3,5 +3,5 @@ input = int(input('Enter a number less than '))
 if input > 25:
     print('Error')
 else:
-    for i in range(input, 25, 1):
+    for i in range(input, 26, 1):
         print('Inside the loop, my variable is',i)
